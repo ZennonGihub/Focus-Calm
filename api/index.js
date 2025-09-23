9997e281fd4dcd9d4e622383f181c4522afc7633
Zennon <zeitaa34@gmail.com>
2025-09-23 12:47:40 -0300

    Primer deploy

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
-// En tu nuevo archivo 'api/index.js'
+// index.js
 import dotenv from "dotenv";
 dotenv.config();
 
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
-import { connectDb } from "../db/db.js";
-import app from "../app.js";
+import { connectDb } from "./db/db.js";
+import app from "./app.js";
 import { checkApiKey } from "./middlewares/auth.middleware.js";
 
 connectDb();
 
-const swaggerDocument = YAML.load("../openapi.yaml");
+const swaggerDocument = YAML.load("./openapi.yaml");
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Welcome to your API!", version: "1.0.0" });
 });
 
 app.use(checkApiKey);
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
