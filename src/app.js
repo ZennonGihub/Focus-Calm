8936a396dd89d9658f2d221c7c5fa129dd95efdd
Zennon <zeitaa34@gmail.com>
2025-10-04 13:56:04 -0300

    ajuste deploy vercel

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import routerApi from "./router/index.router.js";
 
@@ -10,5 +11,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 routerApi(app);
+
+const whitelist = ["http://127.0.0.1:5500", "https://focuscalm.vercel.app"];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("no permitido"));
+    }
+  },
+};
+
+app.use(cors(options));
 
 export default app;
