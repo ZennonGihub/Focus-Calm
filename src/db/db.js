cab4f9eb955b885d8c1b1200f864e87ae9db956b
Zennon <zeitaa34@gmail.com>
2025-10-04 16:53:44 -0300

    ajuste deploy vercel

--- src/db/db.js
+++ src/db/db.js
@@ -3,6 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const uri = process.env.URI_DB;
+// Temporalmente, dentro de tu archivo de conexión:
+
+console.log(
+  "URI_DB cargada (solo mostrar los primeros 20 caracteres):",
+  uri ? uri.substring(0, 20) : "URI NO CARGADA"
+);
 
 export const connectDb = async () => {
   try {
