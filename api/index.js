515b969ed27154744921a4816630590509f4c8a5
Zennon <zeitaa34@gmail.com>
2025-10-04 16:58:47 -0300

    ajuste deploy vercel

--- api/index.js
+++ api/index.js
@@ -7,10 +7,19 @@ import app from "../src/app.js";
 
 connectDb();
 
+const uri = process.env.URI_DB;
+// Temporalmente, dentro de tu archivo de conexión:
+
+console.log(
+  "URI_DB cargada (solo mostrar los primeros 20 caracteres):",
+  uri ? uri.substring(0, 20) : "URI NO CARGADA"
+);
+
 const swaggerDocument = YAML.load("./openapi.yaml");
 
 app.get("/", (req, res) => {
   res.json({ message: "API FOCUS CALM", version: "1.0.0" });
+  console.log;
 });
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
