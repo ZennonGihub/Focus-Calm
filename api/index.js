34c150fa72e5338bc9faf65142971550fe238c22
Zennon <zeitaa34@gmail.com>
2025-10-05 09:51:03 -0300

    ajuste deploy vercel

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
 import app from "../src/app.js";
-import connectDBMiddleware from "./../src/db/db.js";
+import { connectDb } from "./../src/db/db.js";
 
 const swaggerDocument = YAML.load("./openapi.yaml");
 
-app.use(connectDBMiddleware);
+app.use(connectDb);
 app.get("/", (req, res) => {
   res.json({ message: "API FOCUS CALM", version: "1.0.0" });
 });
