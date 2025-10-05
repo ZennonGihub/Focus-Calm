7805e7dee6922e8c70436a33d198b10c68542a6e
Zennon <zeitaa34@gmail.com>
2025-10-05 11:06:54 -0300

    ajuste deploy vercel

--- src/app.js
+++ src/app.js
@@ -4,25 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import routerApi from "./router/index.router.js";
 import { connectDb } from "./db/db.js";
-
 const app = express();
-
-const connectDBMiddleware = async (req, res, next) => {
-  try {
-    await connectDb();
-    next();
-  } catch (error) {
-    console.error("Error en el middleware de DB:", error);
-    res.status(500).json({ message: "Database connection failed." });
-  }
-};
-
-app.use(connectDBMiddleware);
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(morgan("dev"));
-routerApi(app);
 
 const whitelist = ["http://127.0.0.1:5500", "https://focuscalm.vercel.app"];
 const options = {
@@ -34,7 +16,23 @@ const options = {
     }
   },
 };
-
 app.use(cors(options));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+const connectDBMiddleware = async (req, res, next) => {
+  try {
+    await connectDb();
+    next();
+  } catch (error) {
+    console.error("Error en el middleware de DB:", error);
+    res.status(500).json({ message: "Database connection failed." });
+  }
+};
+app.use(connectDBMiddleware);
+
+routerApi(app);
 
 export default app;
