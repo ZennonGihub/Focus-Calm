99e96a4e0a79c205f71880a8dedd947b0ae6c4b5
Zennon <zeitaa34@gmail.com>
2025-10-15 11:04:00 -0300

    Solucion error db vercel

--- src/libs/strategies/passport.local.js
+++ src/libs/strategies/passport.local.js
@@ -10,15 +10,22 @@ const localStrategy = new Strategy(
   },
   async (email, password, done) => {
     try {
+      console.log("1. Buscando usuario en la base de datos...");
       const user = await User.findOne({ email });
+      console.log("2. Búsqueda de usuario finalizada.");
 
       if (!user) {
         return done(boom.unauthorized("Usuario o contraseña inválidos"), false);
       }
+
+      console.log("3. Iniciando comparación de contraseña (bcrypt)...");
       const compare = await bcrypt.compare(password, user.password);
+      console.log("4. Comparación de contraseña finalizada.");
+
       if (!compare) {
         return done(boom.unauthorized("Usuario o contraseña inválidos"), false);
       }
+
       return done(null, user);
     } catch (error) {
       done(error, false);
