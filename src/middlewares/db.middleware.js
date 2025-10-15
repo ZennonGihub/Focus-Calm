f7c8800378f2614ac8fb960822bcc47455a01cf3
Zennon <zeitaa34@gmail.com>
2025-10-15 10:52:24 -0300

    Solucion error db vercel

--- src/middlewares/db.middleware.js
+++ src/middlewares/db.middleware.js
@@ -3,6 +3,7 @@ import { connectDb } from "../db/db.js";
 export const dbMiddleware = async (req, res, next) => {
   try {
     await connectDb();
+    next();
   } catch (error) {
     return res.status(500).json({ message: "Database connection error" });
   }
